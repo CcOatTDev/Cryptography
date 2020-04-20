9bade6246a13fd54277bc6775a70105373c851ed
chatchaiTi <chatchai.ti@hengleasing.co.th>
2020-04-20 11:08:36 +0700

    add source

---
+++ AsymmetricKeyCryptography/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AsymmetricKeyCryptography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainText = "Hello World!";
+            UnicodeEncoding byteConverter = new UnicodeEncoding();
+            byte[] data = byteConverter.GetBytes(plainText);
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                RSAParameters publicKey = rsa.ExportParameters(false);
+                RSAParameters privateKey = rsa.ExportParameters(true);
+
+                byte[] encrypted = Encrypt(data, publicKey, false);
+                byte[] decrypted = Decrypt(encrypted, privateKey, false);
+
+                Console.WriteLine("PlainText: {0}", plainText);
+                Console.WriteLine("Decrypted: {0}", byteConverter.GetString(decrypted));
+            }
+
+            Console.ReadKey();
+        }
+
+        private static byte[] Encrypt(byte[] data, RSAParameters publicKey, bool fOAEP)
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(publicKey);
+
+                return rsa.Encrypt(data, fOAEP);
+            }
+        }
+
+        private static byte[] Decrypt(byte[] encrypted, RSAParameters privateKey, bool fOAEP)
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(privateKey);
+
+                return rsa.Decrypt(encrypted, fOAEP);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace SymmetricKeyCryptography
+{
+    class Program
+    {
+        // Symmetric Key Cryptography 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string plainText = "Hello World!"; //Console.ReadLine().ToString();
+
+            using (AesCryptoServiceProvider aes = new AesCryptoServiceProvider())
+            {
+                byte[] encrypted = Encrypt(plainText, aes.Key, aes.IV);
+                string decrypted = Decrypt(encrypted, aes.Key, aes.IV);
+
+                Console.WriteLine("PlainText: {0}", plainText);
+                Console.WriteLine("Decrypted: {0}", decrypted);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static byte[] Encrypt(string plainText, byte[] key, byte[] iV)
+        {
+            using (AesCryptoServiceProvider aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = key;
+                aes.IV = iV;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+
+                    return msEncrypt.ToArray();
+                }
+            }
+        }
+
+        private static string Decrypt(byte[] encrypted, byte[] key, byte[] iV)
+        {
+            using (AesCryptoServiceProvider aesAlg = new AesCryptoServiceProvider())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iV;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(encrypted))
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                {
+                    return srDecrypt.ReadToEnd();
+                }
+            }
+        }
+    }
+}
